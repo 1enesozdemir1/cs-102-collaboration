bcfb0e91ac14e43d9ad7edc5bd1195317a0ea7cb
Aybars Buğra Aksoy <aybarsbugraaksoy@gmail.com>
2024-02-08 18:00:00 +0300

    Added random array generator

--- menu.java
+++ menu.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
 public class menu {
     public static void main(String[] args) {
-        System.out.println("helüü");
+        int arraySize;
+        Scanner scanner = new Scanner(System.in);
+        arraySize = scanner.nextInt();
+        int[] array = new int[arraySize];
+        Random random = new Random();
+        for (int i=0;i<arraySize;i++){
+            array[i]= random.nextInt(0,101);
+            System.out.println(array[i]);
+        }
     }
 }
