891f0fa5b4c5f78c717cc66339dac2d06657d234
Aybars Buğra Aksoy <aybarsbugraaksoy@gmail.com>
2024-02-08 18:04:24 +0300

    Small changes

--- menu.java
+++ menu.java
@@ -1,15 +1,17 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class menu {
     public static void main(String[] args) {
         int arraySize;
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the random array: ");
         arraySize = scanner.nextInt();
         int[] array = new int[arraySize];
         Random random = new Random();
-        for (int i=0;i<arraySize;i++){
-            array[i]= random.nextInt(0,101);
-            System.out.println(array[i]);
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = random.nextInt(0, 101);
         }
+        scanner.close();
     }
 }
